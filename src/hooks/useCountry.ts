8206c71eebb179d87894fc811cd7910267cfb96f
Yordani <yalvarezb93@gmail.com>
2024-02-28 15:55:34 +0300

    Styles and responsive

--- src/hooks/useCountry.ts
+++ src/hooks/useCountry.ts
@@ -1,4 +1,19 @@
-export const useCountry = () => {
-    
-}
+import { useState } from "react";
+import { Country } from "../interfaces/GetCountriesResponse";
+import { useEffect } from "react";
 
+export const useCountry = () => {
+  const [countries, setCountries] = useState([] as Country[]);
+
+  const getCountries = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  return { getCountries, countries };
+};
--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useCountry } from '../hooks/useCountry'
+import Card from '../components/Card'
+
 
 export default function Countries() {
+
+  const{countries} = useCountry()
+  
   return (
-    <div>
-      
+    <div className='w-[90%] md:max-w-[1200px] mx-auto mt-10 grid grid-cols-1 md:grid-cols-4 gap-10'>
+      {countries?.map((country) => 
+        <Card key={country.name.common} country={country} />
+      )}
     </div>
   )
 }
