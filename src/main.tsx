c3fef17be427722a0f160ca2722bab69f8768e2a
Yordani <yalvarezb93@gmail.com>
2024-02-28 12:55:44 +0300

    First Commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Countries from './pages/Countries'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Countries />
+  </React.StrictMode>,
+)
