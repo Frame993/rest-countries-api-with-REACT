0c398f3604d59acfd9259ec4ef39670f1762e92a
Yordani <yalvarezb93@gmail.com>
2024-02-29 10:50:57 +0300

    fix region out

--- src/hooks/useCountry.ts
+++ src/hooks/useCountry.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Country, Region } from "../interfaces/GetCountriesResponse";
+import { Country} from "../interfaces/GetCountriesResponse";
 import { useEffect } from "react";
 
 export const useCountry = () => {
