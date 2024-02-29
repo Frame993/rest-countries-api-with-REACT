84fe0117be3b4bcc98399940d807c24f3f9d6ed8
Yordani <yalvarezb93@gmail.com>
2024-02-29 16:08:45 +0300

    Error in properties fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,19 +42,18 @@ export default function Navbar() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-moon-2"
+            className="icon icon-tabler icon-tabler-moon"
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <path stroke="none" d="M0 0h24v24H0z" fill="00000060" />
-            <path d="M16.418 4.157a8 8 0 0 0 0 15.686" />
-            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
           </svg>
           <p>{paragraph}</p>
         </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -15,9 +15,9 @@ export default function Search({ onSearch }: Props) {
         <path
           d="M26.75 26.75L19.388 19.388M19.388 19.388C21.3108 17.4653 22.5 14.809 22.5 11.875C22.5 6.00697 17.743 1.25 11.875 1.25C6.00697 1.25 1.25 6.00697 1.25 11.875C1.25 17.743 6.00697 22.5 11.875 22.5C14.809 22.5 17.4653 21.3108 19.388 19.388Z"
           stroke="#00000060"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
       <input
